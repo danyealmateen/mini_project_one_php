a112c7b9e76ef6e9ec6cf2fb19f6935125cf0c03
Danyeal Mateen <danyeal.mateen@hotmail.com>
2024-01-29 22:34:10 +0100

    asd

--- app/App.php
+++ app/App.php
@@ -1,26 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
-// Your Code
-function getTransactionData()
+function readTransactions($path)
 {
-    //berättar vart filen finns
-    $filepath = '../transaction_files/sample_1.csv';
-    //öppnar och läser - r = read
-    $file = fopen($filepath, 'r');
-
-    $data = [];
-    //fgetcsv inbyggd funktion som tolkar och läser en rad från csv filen och returnerar en array fylld med alla tolkade rader. varje row = en rad/cell i csv-filen
-
-    //FALSE kommer när slutet av filen är nådd eller om det blir fel
-    while (($row = fgetcsv($file)) !== FALSE) {
-        $data[] = $row;
-    }
-    //stänger ner filen
-    fclose($file);
-    return $data;
+    $fileContent = file_get_contents($path);
+    echo $fileContent;
 }
-
-
-$transactionData = fopen('../transaction_files/sample_1.csv', 'r');
--- public/index.php
+++ public/index.php
@@ -4,32 +4,10 @@ declare(strict_types=1);
 
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
-//define = definerar en konstant variabel, första arg = variabel namnet, arg2 = "bas" sökvägen till appen , arg3 app = namnet på katalogen, arg4 directory separator = motsvarar \ 
 define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 require APP_PATH . 'App.php';
 
-$transactionData = getTransactionData();
-
-$totalAmount = 0;
-
-foreach ($transactionData as $amount) {
-    //ersätter dollartecken och kommatecken med en tom sträng!
-    $cleanAmount = str_replace(['$', ','], '', $amount[3]);
-    //konverterar strängen till numeriskt värde!
-    $numericAmount = floatval($cleanAmount);
-
-    $totalAmount += $numericAmount;
-}
-
-echo 'Total Amount:' . " " . $totalAmount;
-
-// echo $totalAmount;
-
-// //För all data
-// foreach ($transactionData as $row) {
-//     //implode används för att skapa en sträng av array elementen, tar två argument, en avgränsare och arrayen i sig.
-//     echo implode(', ', $row) .  '<br>';
-// }
+$transactions = readTransactions(FILES_PATH . 'sample_1.csv');
