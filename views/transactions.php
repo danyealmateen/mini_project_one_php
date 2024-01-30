b19c4291fd7996258cec7314bae894239cad6a21
Danyeal Mateen <danyeal.mateen@hotmail.com>
2024-01-30 13:12:13 +0100

    all data rendered correctly

--- views/transactions.php
+++ views/transactions.php
@@ -41,31 +41,41 @@
             <?php
             $transactions = readTransactions(FILES_PATH . 'sample_1.csv');
             $firstRow = true;
+            $totalIncome = 0;
 
+            //Hoppa över första raden i .csv filen pga dubbla rubriker
             foreach ($transactions as $transaction) {
                 if ($firstRow) {
                     $firstRow = false;
                     continue;
                 }
 
+                if (isset($transaction[3])) {
+                    $amountString = str_replace([',', '$'], '', $transaction[3]);
+                    $amount = floatval($amountString);
+                    if ($amount > 0) {
+                        $totalIncome += $amount;
+                    }
+                }
                 echo "<tr>";
 
                 for ($i = 0; $i <= 3; $i++) {
-
+                    //formaterad datum
                     if ($i === 0 && isset($transaction[$i])) {
                         $originalDate = $transaction[$i];
                         $dateObject = DateTime::createFromFormat("m/d/Y", $originalDate);
+
                         if ($dateObject !== false) {
                             $formattedDate = $dateObject->format("M j, Y");
                             echo "<td>" . htmlspecialchars($formattedDate) . "</td>";
+                        } else {
+                            echo "<td> Ogiltigt datum </td>";
                         }
-                    }
-
-
-                    if (isset($transaction[$i])) {
+                    } elseif ($i !== 0 && isset($transaction[$i])) {
                         echo "<td>" . htmlspecialchars($transaction[$i]) . "</td>";
                     }
                 }
+
                 echo "</tr>";
             }
             ?>
@@ -73,7 +83,7 @@
         <tfoot>
             <tr>
                 <th colspan="3">Total Income:</th>
-                <td><!-- YOUR CODE --></td>
+                <td><?php echo htmlspecialchars(number_format($totalIncome, 2)); ?></td>
             </tr>
             <tr>
                 <th colspan="3">Total Expense:</th>
